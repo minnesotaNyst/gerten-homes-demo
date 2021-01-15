7ac0887ff0cc735d5c096dfb5675beb5ad8175b1
nathan-potts41 <pottsandpans41@hotmail.com>
2021-01-14 18:56:11 -0600

    rough outline of the page

--- assets/js/buy.js
+++ assets/js/buy.js
@@ -1,3 +1,9 @@
+// nav drop down trigger
 $(document).ready(function () {
 	$('.dropdown-trigger').dropdown();
+});
+
+// collapsiable trigger
+$(document).ready(function () {
+	$('.collapsible').collapsible();
 });
