2c96319c794a1b8e30e225abe29a7dbc9f6bc1a2
nathan-potts41 <pottsandpans41@hotmail.com>
2021-01-19 18:27:56 -0600

    fixed nav bar, added design on demand page

--- assets/js/buy.js
+++ assets/js/buy.js
@@ -1,13 +1,4 @@
-// nav drop down trigger
-$(document).ready(function () {
-	$('.dropdown-trigger').dropdown();
-});
-
-// collapsiable trigger
-$(document).ready(function () {
-	$('.collapsible').collapsible();
-});
-
 $(document).ready(function () {
 	$('.sidenav').sidenav();
 });
+
---
+++ assets/js/home-services.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+    $('.slider').slider().height(450);
+});
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+});
