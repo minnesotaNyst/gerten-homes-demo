5b2fb5e712048ebc93c1f260b861196cbd3da472
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-01-31 19:47:00 -0600

    added comment seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,16 @@
-const seedPost = require('./post-seeds')
+const seedPost = require('./post-seeds');
+const seedComment = require('./comment-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-    await sequelize.sync({ force: true });
-    console.log('\n----- DATABASE SYNCED -----\n');
-    await seedPost();
-    console.log('\n----- POST SEEDED -----\n');
-  
-    process.exit(0);
-  };
-  
+	await sequelize.sync({ force: true });
+	console.log('\n----- DATABASE SYNCED -----\n');
+	await seedPost();
+	console.log('\n----- POST SEEDED -----\n');
+	await seedComment();
+	console.log('\n----- COMMENTS SEEDED -----\n');
+
+	process.exit(0);
+};
+
 seedAll();
