d107f596fd0159a8d56f2cc7e682b13f6b332981
nathan-potts41 <pottsandpans41@hotmail.com>
2021-02-02 13:33:43 -0600

    added login page

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // set up Handlebars.js as your app's template engine
 app.engine('handlebars', hbs.engine);
