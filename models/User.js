dc8785ebf66c761e2c19ac102c0cf44b90323e11
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-02-06 16:55:19 -0600

    signup form is working

--- models/User.js
+++ models/User.js
@@ -31,7 +31,9 @@ User.init(
 			allowNull: false
 		},
 		first_name: {
-			type: DataTypes.STRING
+			type: DataTypes.STRING,
+			// if no first_name, default the value to null
+			defaultValue: null
 		},
 		last_name: {
 			type: DataTypes.STRING
@@ -86,89 +88,3 @@ User.init(
 );
 
 module.exports = User;
-
-/*
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
-
-module.exports = function (sequelize, DataTypes) {
-	// create our Post model
-	// class User extends Model {
-	// 	// set up method to run on instance data (per user) to check password
-	// 	// checkPassword(loginPw) {
-	// 	// 	return bcrypt.compareSync(loginPw, this.password);
-	// 	// }
-	// }
-	// create fields/columns for Post model
-	const User = sequelize.define(
-		'User',
-		{
-			id: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-				primaryKey: true,
-				autoIncrement: true
-			},
-			username: {
-				type: DataTypes.STRING,
-				allowNull: false
-			},
-			first_name: {
-				type: DataTypes.STRING,
-				allowNull: false
-			},
-			last_name: {
-				type: DataTypes.STRING,
-				allowNull: false
-			},
-			email: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				unique: true,
-				validate: {
-					isEmail: true
-				}
-			},
-			password: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				validate: {
-					len: [4]
-				}
-			},
-			survey_query: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				validate: {
-					max: {
-						args: 80,
-						msg: 'Please use a maximum of 80 characters'
-					}
-				}
-			}
-		},
-		{
-			sequelize,
-			timestamps: true,
-			freezeTableName: true,
-			underscored: true,
-			tableName: 'user'
-		}
-	);
-
-	// checking for unhashed password entered user
-	// comparing to hashed password stored in database
-	User.prototype.checkPassword = function (password) {
-		return bcrypt.compareSync(password, this.password);
-	};
-	// before user account created password automatically hashed
-	User.addHook('beforeCreate', user => {
-		user.password = bcrypt.hashSync(
-			user.password,
-			bcrypt.genSaltSync(10),
-			null
-		);
-	});
-	return User;
-};*/
